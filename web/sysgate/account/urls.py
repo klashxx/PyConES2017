dbd870fdbbe8eafac12697dbcde745ee879aef08
klashxx <klashxx@gmail.com>
2017-08-13 20:10:21 +0200

    class-based views

--- web/sysgate/account/urls.py
+++ web/sysgate/account/urls.py
@@ -3,14 +3,17 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^$', views.Home.as_view(), name='account'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^registro/$', views.registro, name='registro'),
-    url(r'^password_reset/$', auth_views.password_reset, name='reset'),
+    url(r'^password_reset/$',
+        auth_views.PasswordResetView.as_view(),
+        name='reset'),
     url(r'^password_reset/done/$',
-        auth_views.password_reset_done,
+        auth_views.PasswordResetDoneView.as_view(),
         name='password_reset_done'),
     url(r'^password_change/$',
         auth_views.PasswordChangeView.as_view(),
