a9511e9f4e252d028fef728ffeef0afcd25f7032
klashxx <klashxx@gmail.com>
2017-09-10 17:32:55 +0200

    group testing in view

--- web/sysgate/apps/metrics/views.py
+++ web/sysgate/apps/metrics/views.py
@@ -2,26 +2,30 @@ import requests
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
-from sysgate.apps.core.permissions import EsSuperOTienePermisosPorGrupo
+from sysgate.apps.core.permissions import (EsSuperOTienePermisosPorGrupo,
+                                           super_o_esta_en_grupo)
 
 from .models import Metrica
 from .serializers import SerializerMetricas
 
 
 @method_decorator(login_required, name='dispatch')
-class Home(View):
+class Home(UserPassesTestMixin, View):
+    def test_func(self):
+        return super_o_esta_en_grupo(self.request.user, grupo='pycones_gr')
+
     def get(self, request, *args, **kwargs):
         r = requests.get('http://sysgate:8000/metrics/api/v1/metricas/')
         return render(request,
                       'metrics/home.html',
                       context={'metricas': r.json()})
-
 
 class MetricasViewSet(viewsets.ModelViewSet):
     grupos_autorizados = settings.GR_AUTH_METRICS
