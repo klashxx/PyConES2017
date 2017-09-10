509806f9e05162c06db9e9a7455d7e80a8c5eb22
klashxx <klashxx@gmail.com>
2017-09-10 22:45:55 +0200

    Account frontend

--- web/sysgate/account/views.py
+++ web/sysgate/account/views.py
@@ -1,19 +1,22 @@
 from django.conf import settings
 from django.contrib import auth, messages
+from django.contrib.auth import login as login_auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import login as django_login
+from django.contrib.auth.views import login as login_view
 from django.shortcuts import redirect, render
+from django.template.response import TemplateResponse
 from django.views.generic import View
 
 from .forms import LoginForm, RegistroForm
+from .models import User
 
 
 def login(request):
     kwargs = {
         'template_name': 'account/login.html',
         'authentication_form': LoginForm}
-    return django_login(request,
+    return login_view(request,
                         extra_context={'site_name': settings.SITE_NAME,
                                        'title': 'Autenticación'},
                         **kwargs)
@@ -29,16 +32,17 @@ def registro(request):
         form = RegistroForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            django_login(request, user)
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            login_auth(request, new_user)
+            messages.info(request, 'Gracias por registrarse. Sesión abierta')
             return redirect(settings.LOGIN_REDIRECT_URL)
     else:
         form = RegistroForm()
     return render(request, 'account/registro.html', {'form': form})
 
 
-class Home(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'account/home.html')
+@login_required
+def profile(request):
+    ctx = {'usuario': User.objects.get(id=request.user.id)}
+    return TemplateResponse(request, 'account/profile.html', ctx)
