7099e3a0bff3db929fb981d2cd419befe9dd54d6
klashxx <klashxx@gmail.com>
2017-09-11 18:33:49 +0200

    Breadcrumbs

--- web/sysgate/settings.py
+++ web/sysgate/settings.py
@@ -84,6 +84,7 @@ context_processors = [
     'django.template.context_processors.request',
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
+    'sysgate.apps.core.context_processors.breadcrumbs'
 ]
 
 TEMPLATES = [
